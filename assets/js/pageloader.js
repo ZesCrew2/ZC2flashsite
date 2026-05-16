da8a37518d5a107d22d66ff47d374d6e0d2b6544
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2026-05-16 18:30:55 +0930

    Fix sound error

--- assets/js/pageloader.js
+++ assets/js/pageloader.js
@@ -1,14 +1,30 @@
 var currentStage = null;
 var currentPage = null;
+var currentLoader = null;
+var pageLoadToken = 0;
+var pageSoundsRegistered = {};
+
+function registerPageSound(src, id) {
+	if (!createjs.Sound || pageSoundsRegistered[id]) return;
+	pageSoundsRegistered[id] = true;
+	createjs.Sound.registerSound(src, id);
+}
 
 function loadPage(color) {
 	if (typeof createjs === 'undefined') return;
+	pageLoadToken++;
 
 	var flashContent = document.getElementById('flashContent');
 
 	if (currentStage) {
 		createjs.Ticker.removeEventListener("tick", currentStage);
 		currentStage = null;
+	}
+
+	if (currentLoader) {
+		currentLoader.removeAllEventListeners();
+		currentLoader.close();
+		currentLoader = null;
 	}
 
 	var oldScript = document.getElementById('page-script');
@@ -28,23 +44,40 @@ function loadPage(color) {
 	var script = document.createElement('script');
 	script.id = 'page-script';
 	script.src = 'assets/swf/pages/' + color + '.js';
-	script.onload = function() { if (currentPage === color) initPage(color); };
+	var token = pageLoadToken;
+	script.onload = function() {
+		if (currentPage === color && pageLoadToken === token) initPage(color, token);
+	};
 	document.body.appendChild(script);
 }
 
-function initPage(color) {
+function initPage(color, token) {
 	var canvas = document.getElementById('canvas');
 	var anim_container = document.getElementById('animation_container');
 	if (!canvas || !anim_container) return;
 	var comp = AdobeAn.getComposition("B325F180281AD548AF0E7778EAE237A2");
 	var lib = comp.getLibrary();
-	var manifest = lib.properties.manifest.map(function(item) {
-		return { src: 'assets/swf/pages/' + item.src.replace(/\?.*$/, ''), id: item.id };
+	var manifest = [];
+	lib.properties.manifest.forEach(function(item) {
+		var cleanSrc = item.src.replace(/\?.*$/, '');
+		var fullSrc = 'assets/swf/pages/' + cleanSrc;
+		if (/^sounds\//.test(cleanSrc)) {
+			registerPageSound(fullSrc, item.id);
+			return;
+		}
+		manifest.push({ src: fullSrc, id: item.id });
 	});
 	var loader = new createjs.LoadQueue(false);
-	loader.installPlugin(createjs.Sound);
-	loader.addEventListener("fileload", function(evt) { handlePageFileLoad(evt, comp); });
-	loader.addEventListener("complete", function(evt) { handlePageComplete(evt, comp, color); });
+	currentLoader = loader;
+	loader.addEventListener("fileload", function(evt) {
+		if (token !== pageLoadToken || loader !== currentLoader) return;
+		handlePageFileLoad(evt, comp);
+	});
+	loader.addEventListener("complete", function(evt) {
+		if (token !== pageLoadToken || loader !== currentLoader) return;
+		currentLoader = null;
+		handlePageComplete(evt, comp, color);
+	});
 	loader.loadManifest(manifest);
 }
 
