2aa94f47c2345be335ed5c933043c9766da4d9ea
Lily (ZesCrew2) <75373543+ZesCrew2@users.noreply.github.com>
2026-05-17 15:26:12 -0400

    more songs

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -5,13 +5,16 @@ window.musicPlaylist = [
     { path: 'assets/music/Animal Crossing - Town Hall (Pelly).mp3', name: 'Animal Crossing - Town Hall (Pelly)' },
     { path: 'assets/music/EarthBound - Your Name, Please (Noiseless).mp3', name: 'EarthBound - Your Name, Please (Noiseless)' },
     { path: 'assets/music/QT - Main Song.mp3', name: 'QT - Main Song' },
+    { path: 'assets/music/blue switch palace - super mario world [SPC700] (fantrack) - thorns.mp3', name: 'Thorns - Blue Switch Palace [SPC700]' },
     { path: 'assets/music/Lily (ZesCrew2) - Bejuel (Inst).mp3', name: 'Lily (ZesCrew2) - Bejuel (Inst)' },
     { path: 'assets/music/All The Things She Said (Sped Up, Inst).mp3', name: 't.A.T.u. - All The Things She Said (Sped Up, Inst)' },
     { path: 'assets/music/Kalimba.mp3', name: 'Mr. Scruff - Kalimba' },
     { path: 'assets/music/weegee.mp3', name: 'sephfire, SGX - Mario Paint "Intense Color" OC ReMix' },
     { path: 'assets/music/Drown in the Now.mp3', name: 'The Crystal Method (Feat. Matisyahu) - Drown in the Now' },
+    { path: 'assets/music/Somebody's Watching Me (Instrumental).mp3', name: 'Mysto & Pizzi - Somebody's Watching Me (Instrumental)' },
+    { path: 'assets/music/Let's Kill Tonight (Instrumental).mp3', name: 'Panic! at the Disco - Let's Kill Tonight (Instrumental)' },
     { path: 'assets/music/Your Game is Over (Instrumental).mp3', name: 'MiatriSs - Your Game is Over (Instrumental)' },
-    { path: 'assets/music/blue switch palace - super mario world [SPC700] (fantrack) - thorns.mp3', name: 'Thorns - Blue Switch Palace [SPC700]' }
+    { path: 'assets/music/seven color generator inst.mp3', name: '渡辺未来 - 七色ジェネレーター (Off-Vocal)' }
 ];
 
 (function() {
